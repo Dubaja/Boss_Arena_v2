753f6dc937a911ef44de3820a2352fea8d2d5e19
Zarko <zardubajic@gmail.com>
2020-04-18 23:56:56 +0200

    pwup animation
    added new sprite and animation to the powerup

--- Boss_Arena/Assets/Scripts/PowerUp.cs
+++ Boss_Arena/Assets/Scripts/PowerUp.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class PowerUp : MonoBehaviour
 {
+    public GameObject anim;
+
     void OnTriggerEnter2D(Collider2D collider){
         if(collider.tag == "Player"){
-            //instantiate animation
+            GameObject eff = Instantiate(anim, transform.position, Quaternion.identity);
+            Destroy(eff, 0.5f);
             FindObjectOfType<Buff>().powerUpFlag = true;
             Destroy(gameObject);
         }
